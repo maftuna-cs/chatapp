679aa21ff6b23fc63fab0855dd4db2056f266d2a
Maftuna <kh.maftu@gmail.com>
2020-04-25 18:05:35 +0900

    initial state added for topic

--- chats/src/Store.js
+++ chats/src/Store.js
@@ -3,9 +3,64 @@ import { checkPropTypes } from 'prop-types';
 
 const CTX = React.createContext();
 
+/*
+
+from: 'user'
+msg: 'hi'
+topic: 'general'
+
+
+state {
+    topic1: [
+        {msg}, {msg}, {msg}
+    ]
+
+    topic2: [
+        {msg}, {msg}, {msg}
+    ]
+}
+*/
+
+
+const initState = {
+    general: [
+        {from: 'pianoclass', msg: 'hello'},
+        {from: 'artclass', msg: 'hello'},
+        {from: 'mathclass', msg: 'hello'}
+    ],
+    topic2: [
+        {from: 'pianoclass', msg: 'hello'},
+        {from: 'pianoclass', msg: 'hello'},
+        {from: 'pianoclass', msg: 'hello'}
+    ],
+    
+}
+
 function reducer(state, action) {
+
+    const {from, msg, topic} = action.payload;
+
     switch(action.type) {
+
+        case 'RECEIVE_MESSAGE': 
         
+        return {
+
+            ...state,
+            [topic]: [
+                ...state,[topic],
+
+                {from,msg}
+            ]
+                
+
+        
+
+        }
+        default:
+            return state
+        
+
     }
 }
 
@@ -14,7 +69,7 @@ export default function Store(props) {
     const reducerHook = React.useReducer(reducer, initState);
 
     return (
-        <CTX.Provider value={}>
+        <CTX.Provider value={reducerHook}>
             {props.children}
         </CTX.Provider>
     )
